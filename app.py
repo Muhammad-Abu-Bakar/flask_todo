cfa4bfd004b22537801a4bd8bdae9565f87a21bf
Muhammad-Abu-Bakar <abubakarg28@gmail.com>
2024-07-20 17:21:50 +0100

    new configration have been added to the app.py file for supporting MySql

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
+from config import Config
+from models import db, TodoItem
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
-# Dummy database
-todos = []
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def index():
+    todos = TodoItem.query.all()
     return render_template('index.html', todos=todos)
 
 @app.route('/add', methods=['POST'])
 def add():
     task = request.form.get('task')
     if task:
-        todos.append(task)
+        new_todo = TodoItem(task=task)
+        db.session.add(new_todo)
+        db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
-    if 0 <= task_id < len(todos):
-        todos.pop(task_id)
+    todo = TodoItem.query.get_or_404(task_id)
+    db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
